2909ab9cb78a8b5dd5f48a291d090ab2ecf3d0e9
adityajatla <adityajatlaofficial@gmail.com>
2025-01-01 21:49:26 +0530

    data types added

---
+++ 07-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("og" "panja" "darling")
+
+echo "First Movie: ${MOVIES[0]}"
+echo "Second Movie: ${MOVIES[1]}"
+echo "Third Movie: ${MOVIES[2]}"
+
+
+echo "All Movies : ${MOVIES[@]}"
