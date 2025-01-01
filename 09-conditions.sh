a2cc71adb9e2ab00d7a73706324eef8dde6c88e9
adityajatla <adityajatlaofficial@gmail.com>
2025-01-01 22:28:29 +0530

    Conditions Added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NUMBER=$1
 #-gt(greater than),-lt(less than),-eq(equal to),-ge(greater than equal to),-le(less than equal to)
-if [ $NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
     echo "Given Number is Greater than 100"
 else
