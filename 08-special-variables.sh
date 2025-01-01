9386dbfc43dea3256d08aaa53ce4ecf88e2cad39
adityajatla <adityajatlaofficial@gmail.com>
2025-01-01 22:05:00 +0530

    special variables added

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,4 +7,6 @@ echo "Present Working Directory: $PWD"
 echo "Home Directory of Current User: $HOME"
 echo "Which user is running the script: $USER"
 echo "Process id of current script: $$"
+sleep 60 #run in foreground
+sleep 60 & #run in backgroung
 echo "Process id of last command in the background: $!"
