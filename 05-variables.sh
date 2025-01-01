c92e3439ba3b83d7682652bee1dc71ba15698c87
adityajatla <adityajatlaofficial@gmail.com>
2025-01-01 20:40:27 +0530

    Variables added

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,12 @@
 
 echo "Please enter username:"
 
-read USERNAME
+read -s USERNAME 
 
 echo "username entered is :$USERNAME "
+
+echo "enter password"
+
+read -s PASSWORD
+
+echo "Password is :$PASSWORD"
