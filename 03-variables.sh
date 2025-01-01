066fc0504dc1daa84f7b44037582607f7ee3479f
adityajatla <adityajatlaofficial@gmail.com>
2025-01-01 20:26:40 +0530

    Variables added

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PERSON1=Babji
-PERSON2=Bhavani
+PERSON1=Prabhas
+PERSON2=Bujji
 
 echo "$PERSON2: Hi $PERSON1"
-echo "$PERSON1: Hi $PERSON2"
+echo "${PERSON1}: Hi $PERSON2"
 echo "$PERSON2: How are you doing?"
 echo "$PERSON1: I am Good ,What about you?"
