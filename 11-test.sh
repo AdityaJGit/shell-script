cf47ede3103c6c73ceb1c6277f8d4c53db212496
adityajatla <adityajatlaofficial@gmail.com>
2025-01-04 14:48:51 +0530

    Shell Script

---
+++ 11-test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then    
+    echo "ERROR : You must have sudo access to execute the Script"
+    exit 1 #We should give other than 0
+fi
+dnf list installed mysql
+
+if[ $? -ne 0 ] #not installed
+then
+    dnf install mysql -y
+
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Mysql .... FAILURE"
+        exit 1
+    else
+        echo "Installing Mysql......SUCCESS"
+    fi
+else
+    echo "MySQL is already installed...INSTALLED"
+fi
+
+dnf list installed git
+
+if[ $? -ne 0 ]
+then
+    dnf install git -y
+
+    if [$? -ne 0]
+    then 
+        echo "Installing Git ........FAILURE"
+        exit 1
+    else 
+        echo "Installing GIT.........SUCCESS"
+    fi
+else
+    echo "Git is already Installed....INSTALLED"
+fi
+
