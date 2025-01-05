23addf6537bfe67740ec08a1b3e40dc6ad949241
adityajatla <adityajatlaofficial@gmail.com>
2025-01-05 18:28:14 +0530

    assignment

--- transpose.sh
+++ transpose.sh
@@ -2,7 +2,7 @@
 
 # Transpose a file
 transpose_file() {
-    awk '
+    awk 
     {
         for (i = 1; i <= NF; i++) {
             a[i, NR] = $i
@@ -16,11 +16,11 @@ transpose_file() {
             }
         }
     }
-    ' OFS=' ' "$1"
+    ' OFS='  $1
 }
 
 # Input file
-input_file="file.txt"
+input_file= file.txt
 
 # Transpose and output to console
-transpose_file "$input_file"
+transpose_file $input_file
