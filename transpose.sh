3066c2a97da5dc6676c402d1944614bfd00a6ab2
adityajatla <adityajatlaofficial@gmail.com>
2025-01-05 18:32:19 +0530

    assignment

--- transpose.sh
+++ transpose.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-# Transpose a file
+# Function to transpose the content of a file
 transpose_file() {
-    awk 
+    awk '
     {
-        for (i = 1; i <= NF; i++) 
-        {
+        for (i = 1; i <= NF; i++) {
             a[i, NR] = $i
         }
     }
-    NF > maxNF { maxNF = NF }
+    NF > maxNF { maxNF = NF }  # Track maximum number of fields in a row
     END {
-        for (i = 1; i <= maxNF; i++) {
-            for (j = 1; j <= NR; j++) {
+        for (i = 1; i <= maxNF; i++) {  # Loop through columns
+            for (j = 1; j <= NR; j++) {  # Loop through rows
                 printf "%s%s", a[i, j], (j == NR ? ORS : OFS)
             }
         }
     }
-    ' OFS='  $1
-
+    ' OFS=' ' "$1"
+}
 
 # Input file
-input_file= $(file.txt)
+input_file="file.txt"
+
+# Check if the file exists
+if [[ ! -f $input_file ]]; then
+    echo "Error: $input_file not found!"
+    exit 1
+fi
 
 # Transpose and output to console
-transpose_file $input_file
+transpose_file "$input_file"
