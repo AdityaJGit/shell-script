641725d74567ed442921b288b5fb0fa49914ed84
adityajatla <adityajatlaofficial@gmail.com>
2025-01-09 19:06:04 +0530

    Backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -61,6 +61,7 @@ if [ -n "$FILES" ]
 then
     echo "Files are: $FILES"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 else
     echo "No Files found older than $DAYS"
 fi
