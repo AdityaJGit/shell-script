1e3412482ef2f3b94472baa959ccd309ad6c4cd1
adityajatla <adityajatlaofficial@gmail.com>
2025-01-09 19:27:05 +0530

    Backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -8,67 +7,61 @@ N="\e[0m"
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14} #if user not providing no.of.days , we are taking 14 as default 
+DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
 
-LOGS_FOLDER="/home/ec2-user/shellscript-logs/"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-
-
 USAGE(){
-
-    echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 }
 
-mkdir -p /home/ec2-user/shellscript-logs/
+mkdir -p /home/ec2-user/shellscript-logs
 
 if [ $# -lt 2 ]
 then
-  USAGE 
+    USAGE
 fi
 
-if [ ! -d $SOURCE_DIR ]
+if [ ! -d "$SOURCE_DIR" ]
 then
-    echo -e "$SOURCE_DIR Does not exist...Please Check"
+    echo -e "$SOURCE_DIR Does not exist...Please check"
     exit 1
 fi
 
-if [ ! -d $DEST_DIR ]
+if [ ! -d "$DEST_DIR" ]
 then
-    echo -e "$DEST_DIR Does not exist...Please Check"
+    echo -e "$DEST_DIR Does not exist...Please check"
     exit 1
 fi
-
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-echo "Files are $FILES"
-
-if [ -n "$FILES" ]
+if [ -n "$FILES" ] # true if there are files to zip
 then
     echo "Files are: $FILES"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
     find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
     if [ -f "$ZIP_FILE" ]
     then
-        echo -e "Successfully Created zip file for files older than $DAYS"
-        while read -r filepath
+        echo -e "Successfully created zip file for files older than $DAYS"
+        while read -r filepath # here filepath is the variable name, you can give any name
         do
-        echo "Deleting File: $file" &>>$LOG_FILE_NAME
-        rm -rf $filepath
-        echo "Deleted File: $filepath"
-        done <<< $FILES_TO_DELETE
-
-    else    
-        echo -e "$R Error:: $N Failed to create ZIP file"
+            echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+            rm -rf $filepath
+            echo "Deleted file: $filepath"
+        done <<< $FILES
+    else
+        echo -e "$R Error:: $N Failed to create ZIP file "
         exit 1
     fi
-else
-    echo "No Files found older than $DAYS"
-fi
 
+else
+    echo "No files found older than $DAYS"
+fi
